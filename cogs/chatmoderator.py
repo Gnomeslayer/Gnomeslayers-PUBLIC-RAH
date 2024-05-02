88974472a9fa51a128d0e707c9f1d089adc2054f
Declan <declan.otten@live.com.au>
2024-05-02 12:39:43 +1000

    Updated chatmoderator
    The changes can be found in this issue: https://github.com/Gnomeslayer/Gnomeslayers-PUBLIC-RAH/issues/1

--- cogs/chatmoderator.py
+++ cogs/chatmoderator.py
@@ -15,13 +15,18 @@ from datetime import timedelta
 
 class ChatModerator(commands.Cog):
     def __init__(self, bot:commands.Bot, config:dict):
-        
         print("[Cog] ChatModerator has been initiated")
         self.bot:commands.Bot = bot
         self.config:dict = config
         self.fun = MainFunctions(config=config)
         self.embed_factory = EmbedFactory(config=config, bot=bot)
-        
+        self.ignored_roles = {}
+        self.monitor_info = {}
+    
+    async def load_settings(self, guild_id):
+        self.ignored_roles[guild_id] = await self.fun.get_monitor_ignore_role(guild_id=guild_id)
+        self.monitor_info[guild_id] = await self.fun.get_monitor_info(guild_id=guild_id)
+          
     @command(name="monitor", description="Adds a new phrase to monitor in your guild")
     @guild_only()
     @has_permissions(administrator=True)
@@ -52,47 +57,43 @@ class ChatModerator(commands.Cog):
         if isinstance(error, MissingPermissions):
             await ctx.response.send_message(error, ephemeral=True)
     
-    
     @guild_only()
     @commands.Cog.listener()
     async def on_message(self, message: Message):
         if message.author.bot or not message.guild:
             return
-        monitor_info = await self.fun.get_monitor_info(guild_id=message.guild.id)
-        ignored_roles = await self.fun.get_monitor_ignore_role(guild_id=message.guild.id)
-        user_roles = message.author.roles
-        for role in user_roles:
-            if role.id in ignored_roles:
-                return
-        
-        if monitor_info:
-            for info in monitor_info:
+        await self.load_settings(message.guild.id)
+        if self.monitor_info[message.guild.id]:
+            for info in self.monitor_info[message.guild.id]:
                 info:model.Monitor = info
-                ratio = SequenceMatcher(None, info.phrase, message.content).ratio()
-                ratio = ratio * 100
-                ratio = round(ratio,2)
+                ratio = SequenceMatcher(None, info.phrase.lower(), message.content.lower()).ratio() * 100
                 if ratio >= info.match_strength_percent:
-                    if info.delete:
-                        await message.delete()
-                    if info.warn:
-                        try:
-                            await message.author.send(f"# WARNING\n```{info.warn_message}```")
-                        except:
-                            await message.channel.send(f"# WARNING FOR {message.author.mention}\n```{info.warn_message}```")
-                    if info.timeout_user:
-                        try:
-                            duration = timedelta(minutes=int(info.timeout_time))
-                            await message.author.timeout(duration, reason="Auto Moderatored")
-                        except:
-                            print("Unable to time out user...?")
-                    if info.ban:
-                        try:
-                            await message.author.ban(delete_message_days=7, delete_message_seconds=604800, reason="Auto Moderatored")
-                        except:
-                            print("Unable to ban user?")
-                    if info.respond:
-                        await message.channel.send(f"{message.author.mention}```{info.response_message}``` _This is an automatic response_")
+                    if any(role.id in self.ignored_roles[message.guild.id] for role in message.author.roles):
+                        return
+                    await self.process_monitor_actions(message, info)
                     
-                    if info.alert_staff:
-                        await message.channel.send(f"AAHHH")
-                    return
+    async def process_monitor_actions(self, message, info):
+        if info.delete:
+            await message.delete()
+        if info.warn:
+            try:
+                await message.author.send(f"# WARNING\n```{info.warn_message}```")
+            except:
+                await message.channel.send(f"# WARNING FOR {message.author.mention}\n```{info.warn_message}```")
+        if info.timeout_user:
+            try:
+                duration = timedelta(minutes=int(info.timeout_time))
+                await message.author.timeout(duration, reason="Auto Moderatored")
+            except:
+                print("Unable to time out user...?")
+        if info.ban:
+            try:
+                await message.author.ban(delete_message_days=7, delete_message_seconds=604800, reason="Auto Moderatored")
+            except:
+                print("Unable to ban user?")
+        if info.respond:
+            await message.channel.send(f"{message.author.mention}```{info.response_message}``` _This is an automatic response_")
+        
+        if info.alert_staff:
+            await message.channel.send(f"AAHHH")
+        return
